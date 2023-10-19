c2b6080d991992f5991dd02add1fcf2e215bed89
Walid Said <wsaidamin@gmail.com>
2023-10-19 16:50:00 +0300

    Add models, data migrations and item factory

---
+++ database/factories/ItemFactory.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace Database\Factories;
+
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+/**
+ * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Item>
+ */
+class ItemFactory extends Factory
+{
+    /**
+     * Define the model's default state.
+     *
+     * @return array<string, mixed>
+     */
+    public function definition(): array
+    {
+        return [
+            'name' => fake()->words(2, true),
+            'description' => fake()->paragraph(),
+            'price' => fake()->numberBetween(10, 500)
+        ];
+    }
+}

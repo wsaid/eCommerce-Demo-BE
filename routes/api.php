c1aea2c3467ae6133b16b5d9fd47026a829922ed
Walid Said <wsaidamin@gmail.com>
2023-10-22 05:52:15 +0300

    Add api route

--- routes/api.php
+++ routes/api.php
@@ -1,19 +1,19 @@
 <?php
 
+use App\Http\Controllers\CartController;
+use App\Http\Controllers\ItemController;
+use App\Http\Controllers\OrderController;
+use App\Models\Customer;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
-/*
-|--------------------------------------------------------------------------
-| API Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register API routes for your application. These
-| routes are loaded by the RouteServiceProvider and all of them will
-| be assigned to the "api" middleware group. Make something great!
-|
-*/
 
-Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
-    return $request->user();
-});
+Route::get('items', [ItemController::class, 'index']);
+
+Route::get('cart/{customer}', [CartController::class, 'show']);
+
+Route::post('cart/{customer}', [CartController::class, 'store']);
+
+Route::apiResource('cart', CartController::class);
+
+Route::post('place-order/{customer}', [OrderController::class, 'store']);

de55938e92f8179b558336e5db3a9108b16fc42c
Walid Said <wsaidamin@gmail.com>
2023-10-22 11:17:43 +0300

    Fix validation

--- app/Services/CartService.php
+++ app/Services/CartService.php
@@ -3,13 +3,15 @@
 namespace App\Services;
 
 use App\Models\Cart;
+use Illuminate\Http\Response;
+use Illuminate\Validation\ValidationException;
 
 class CartService {
 
     public function updateQuantity(Cart $cart, $quantity)
     {
         if ($cart->quantity + $quantity > 10) {
-            throw new \InvalidArgumentException('Maximum quantity for the same item is 10.');
+            throw ValidationException::withMessages(['Quantity' => 'Maximum quantity for the same item is 10.']);
         }
 
         // won't trigger Model event

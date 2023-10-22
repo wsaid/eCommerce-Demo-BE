639ada00b96c9b00b94a56bef97c1bb722f1db60
Walid Said <wsaidamin@gmail.com>
2023-10-22 10:03:02 +0300

    Customer model

--- app/Models/Customer.php
+++ app/Models/Customer.php
@@ -8,4 +8,14 @@ use Illuminate\Database\Eloquent\Model;
 class Customer extends Model
 {
     use HasFactory;
+
+    public function carts()
+    {
+        return $this->hasMany(Cart::class);
+    }
+
+    public function orders()
+    {
+        return $this->hasMany(Order::class);
+    }
 }
